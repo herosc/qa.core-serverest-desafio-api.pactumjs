2cf440b2920b03f4c4f1a6459a7c5a32215d2398
herosneto <heros.neto@zig.fun>
2024-10-25 15:32:48 -0300

    alterações

--- src/services/users/deleteUsers.service.js
+++ src/services/users/deleteUsers.service.js
@@ -2,6 +2,7 @@ async function deleteUsers(userId, statusCode) {
     const { json } = await spec()
         .delete(`https://serverest.dev/usuarios/${userId}`)
         .expectStatus(statusCode)
+        
     return json
 }
 
